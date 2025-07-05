d6621bfe43a2f9f943a814899150b50f85dddf4c
Gledis Lami <lamigledi@gmail.com>
2025-07-05 14:27:55 +0200

    add change

--- src/main/java/org/sda/util/ApplicationConstants.java
+++ src/main/java/org/sda/util/ApplicationConstants.java
@@ -1,7 +1,7 @@
 package org.sda.util;
 
 public class ApplicationConstants {
-    public static final String API_KEY = "UzOwLg3LMYVvjxc49nzZbLbaIjklr1pX";
+    public static final String API_KEY = "CHANGE IT";
 
     public static final String API_BASE_URL = "http://dataservice.accuweather.com";
 }
